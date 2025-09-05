8343c9b4f4c95314c9e89d4f83b5d26642fc3617
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2025-09-05 22:35:17 +0200

    adds spacing at the bottom

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -29,5 +29,7 @@ export const Layout: FunctionComponent<LayoutProps> = ({
     <div className="container mx-auto flex-1 overflow-hidden p-4 md:p-8 bg-white bg-opacity-50 md:rounded-lg">
       <div className="overflow-y-auto h-full">{children}</div>
     </div>
+
+    <footer className="h-12"></footer>
   </div>
 );
